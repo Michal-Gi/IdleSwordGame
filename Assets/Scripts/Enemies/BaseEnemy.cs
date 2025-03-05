2da55c49c698a36e3970f391b7f0f4b2a3c37e6a
Michal-Gi <michalg394@gmail.com>
2025-03-05 09:32:29 +0100

    Fixed enemy damage type resistances

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class BaseEnemy : MonoBehaviour
 {
+    #region general
     [SerializeField]
     public HealthSystem _healthSystem;
 
@@ -15,27 +18,62 @@ public class BaseEnemy : MonoBehaviour
     [SerializeField]
     public int EXP;
 
+    #endregion
+    [Header("Resistances")]
+    #region Damage Type Resistance
     [SerializeField]
-    public ResistanceDict ResistanceDict;
+    public float BludgeoningResistance = 0;
+    [SerializeField]
+    public float PiercingResistance = 0;
+    [SerializeField]
+    public float SlashingResistance = 0;
+    [SerializeField]
+    public float FireResistance = 0;
+    [SerializeField]
+    public float ColdResistance = 0;
+    [SerializeField]
+    public float NecroticResistance = 0;
+    [SerializeField]
+    public float LightningResistance = 0;
+    [SerializeField]
+    public float PsychicResistance = 0;
+    [SerializeField]
+    public float PoisonResistance = 0;
+    [SerializeField]
+    public float AcidResistance = 0;
+    [SerializeField]
+    public float ThunderResistance = 0;
+    [SerializeField]
+    public float ForceResistance = 0;
+    [SerializeField]
+    public float TrueResistance = 0;
+    [SerializeField]
+    public float RadiantResistance = 0;
 
-    private Dictionary<DamageType, float> DamageResistancyMultiplayers;
+    #endregion
 
-    private void Awake()
-    {
-        DamageResistancyMultiplayers = ResistanceDict.ToDict();
+    public float GetEnemyResistance(DamageType damageType) {
+        switch (damageType)
+        {
+            case DamageType.Bludgeoning: return BludgeoningResistance;
+            case DamageType.Piercing: return PiercingResistance;
+            case DamageType.Slashing: return SlashingResistance;
+            case DamageType.Fire: return FireResistance;
+            case DamageType.Cold: return ColdResistance;
+            case DamageType.Necrotic: return NecroticResistance;
+            case DamageType.Lightning: return LightningResistance;
+            case DamageType.Psychic: return PsychicResistance;
+            case DamageType.Poison: return PoisonResistance;
+            case DamageType.Acid: return AcidResistance;
+            case DamageType.Thunder: return ThunderResistance;
+            case DamageType.Force: return ForceResistance;
+            case DamageType.True: return TrueResistance;
+            case DamageType.Radiant: return RadiantResistance;
+            default: return 0;
+        }
     }
 
     public void GetAttacked(float damage, DamageType damageType) {
-        if(damage < Armor) { damage = Armor; }
-        if (!DamageResistancyMultiplayers.TryGetValue(damageType, out float damageReduction)) {
-            Debug.Log("damage type not found in the list");
-            damageReduction = 0;
-        }
-        else
-        {
-            Debug.Log("Damage type found");
-            damageReduction = DamageResistancyMultiplayers[damageType];
-        }
-        _healthSystem.TakeDamage(damage * (1 - damageReduction) - Armor);
+        _healthSystem.TakeDamage(Math.Max(damage * (1 - GetEnemyResistance(damageType)) - Armor, 0));
     }
 }
